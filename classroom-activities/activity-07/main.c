e1051a6f9d750157927b8bd13364587f395886bc
gabrielauzier <gabriel@loldesign.com.br>
2023-02-05 17:59:34 -0400

    feat: add exercises from activity 07

--- classroom-activities/activity-07/main.c
+++ classroom-activities/activity-07/main.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 #define GET_INT_LEN(x) (sizeof(x) / sizeof((x)[0]))
-#define NUMS_LEN 5
+#define NUMS_LEN 7
 
 void exercise01() {
     int nums[NUMS_LEN];
@@ -39,13 +39,48 @@ void exercise03() {
 
     printf("Len of is %lld", GET_INT_LEN(nums));
 }
+
 //
 //int reverse_array (int nums[]) {
 //
 //}
 
+void printArray(int *array, int size) {
+    int i = 0;
+    printf("Array = ");
+
+    while (i < size) {
+        printf("%d ", *array);
+        array++; i++;
+    }
+    printf("\n");
+}
+
+void exercise06() {
+    int array[NUMS_LEN] = {7,90,1,3,0, 100 ,2};
+
+    printArray(array, NUMS_LEN);
+    int i, j;
+    for (i = 0; i < NUMS_LEN; i++) {
+        int lowest = array[i], lowestIndex = i;
+
+        for (j = i; j < NUMS_LEN; j++) {
+            if (array[j] < lowest) {
+                lowest = array[j];
+                lowestIndex = j;
+            }
+        }
+
+        int aux = array[i];
+        array[i] = array[lowestIndex];
+        array[lowestIndex] = aux;
+
+        printArray(array, NUMS_LEN);
+    }
+}
+
 int main() {
-    exercise03();
+    exercise06();
 
     return 0;
 }
